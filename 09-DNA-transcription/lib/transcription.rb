ed8c28f26e0da2f4cf1d77fa5bb8ab759d34fdf1
Edward Stevinson <edward.stevinson@hotmail.com>
2017-04-27 17:00:52 +0100

    Passes all tests

--- 09-DNA-transcription/lib/transcription.rb
+++ 09-DNA-transcription/lib/transcription.rb
@@ -1,4 +1,22 @@
-def dna_transcription(dna_sequence)
-  # TODO: return the RNA complement of the incoming DNA sequence
+require "pry-byebug"
 
+DNA_TO_RNA = {
+  G: "C",
+  C: "G",
+  T: "A",
+  A: "U"
+}
+
+def dna_transcription(dna_sequence)
+  transformed_array = dna_sequence.map do |dna|
+    if DNA_TO_RNA.has_key?(dna.upcase.to_sym)
+      DNA_TO_RNA[dna.upcase.to_sym]
+    else
+      "*"
+    end
+  end
+  transformed_array.delete("*")
+  return transformed_array
 end
+
+
