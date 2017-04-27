8c5bbcb0144c09d939452aa94be6b9e052ba3901
Edward Stevinson <edward.stevinson@hotmail.com>
2017-04-27 17:14:14 +0100

    Passes all tests

--- 11-Scrabble-counter/lib/scrabble_counter.rb
+++ 11-Scrabble-counter/lib/scrabble_counter.rb
@@ -4,7 +4,13 @@ SCRABBLE_POINTS = { "A"=>1, "B"=>3, "C"=>3, "D"=>2,
   "Q"=>10, "R"=>1, "S"=>1, "T"=>1, "U"=>1, "V"=>4,
   "W"=>4, "X"=>8, "Y"=>4, "Z"=>10 }
 
+# Return the total number of points scored for the sentencec based on the hash above
 def scrabble_counter(sentence)
-  # TODO: return the total number of points scored for the sentencec based on the hash above
-
+  # Keep track of our score
+  score = 0
+  # Remove everything apart from letters
+  array_of_letters = sentence.upcase.gsub(/[.! ]/, "").split("")
+  array_of_letters.each { |letter| score += SCRABBLE_POINTS[letter] }
+  return score
 end
+
