2edfb05de7ab3d791dd472eac8fcacc145ff7c0c
Edward Stevinson <edward.stevinson@hotmail.com>
2017-04-27 16:27:50 +0100

    Can now handle apostrophes

--- 07-Word-frequency/lib/word_frequency.rb
+++ 07-Word-frequency/lib/word_frequency.rb
@@ -1,6 +1,9 @@
 # TODO: return a hash representing the number of apperances of words in the sentence
 def word_frequency(sentence)
   count_words = Hash.new(0)
-  sentence.downcase.split(" ").each { |w| count_words[w.downcase.gsub(/[.,\:\-\"\']/, "")] += 1 }
+  sentence.downcase.split(" ").each { |w| count_words[w.downcase.gsub(/\'s/, "").gsub(/[.,\:\-\"\']/, "")] += 1 }
   return count_words
 end
+
+
+
