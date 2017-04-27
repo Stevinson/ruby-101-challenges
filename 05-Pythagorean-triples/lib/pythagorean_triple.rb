02ae647e590aa7955e71231e561a785828e20f51
Edward Stevinson <edward.stevinson@hotmail.com>
2017-04-27 15:40:25 +0100

    Passes all tests

--- 05-Pythagorean-triples/lib/pythagorean_triple.rb
+++ 05-Pythagorean-triples/lib/pythagorean_triple.rb
@@ -1,4 +1,10 @@
+# Return true/false depending if numbers a, b and c are Pythagorean triples
 def pythagorean_triple?(a, b, c)
-  # TODO: return true/false depending if numbers a, b and c are Pythagorean triples
-
+  # Check if the provided arguments are all Fixnums
+  [a, b, c].all? { |arg| arg.class == Fixnum } ? true : (return false)
+# Put the
+  sorted_nums = [a, b, c].sort
+    # Check if they are triples
+  return sorted_nums[0]^2 == sorted_nums[1]^2 + sorted_nums[2]^2 ? true : false
 end
+
