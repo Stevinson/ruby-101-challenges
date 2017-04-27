6cfcd526e3657c83012cd42230b7aac25594427e
Edward Stevinson <edward.stevinson@hotmail.com>
2017-04-27 15:46:04 +0100

    Passes all tests

--- 06-FizzBuzz/lib/fizzbuzz.rb
+++ 06-FizzBuzz/lib/fizzbuzz.rb
@@ -1,4 +1,18 @@
+# Play the game FizzBuzz
 def fizzbuzz(max)
-  # TODO: return an Array
-
+  # Number has to be positive
+  return [] if max < 1
+  array = []
+  (1..max).each do |i|
+    if (i % 3).zero? && (i % 5).zero?
+      array.push('FizzBuzz')
+    elsif (i % 5).zero?
+      array.push('Buzz')
+    elsif (i % 3).zero?
+      array.push('Fizz')
+    else
+      array.push(i)
+    end
+  end
+  return array
 end
